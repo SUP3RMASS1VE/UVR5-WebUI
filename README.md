dd439ba8dfd77a483a11fb3c6d633935b4e50b8b
SUP3RMASS1VE <Martincull@live.com>
2024-12-06 17:01:58 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# UVR5-WebUI
+
+A pinokio script for https://github.com/Eddycrack864/UVR5-UI
+
---
+++ install.js
@@ -0,0 +1,42 @@
+module.exports = {
+  run: [
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/SUP3RMASS1VE/UVR5-UI app",  // Clone the GitHub repository
+        ]
+      }
+    },
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",  // Virtual environment folder path
+          path: "app",  // Path to start the shell from
+        }
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",  // Virtual environment folder path
+        path: "app",  // Path to start the shell from
+        message: [
+          "pip install --no-input gradio==5.7.1",
+          "pip install --no-input scipy",
+          "pip install --no-input yt-dlp", 
+          "pip install --no-input gradio_i18n", 
+          "pip install --no-input audio-separator[gpu]==0.24.1" 
+        ]
+      }
+    },
+    {
+      method: "fs.link",
+      params: {
+        venv: "app/env"
+      }
+    },
+  ]
+}
